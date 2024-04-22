208f757da7ad20a461f6e4dc98dde49106897ec8
RaoNaila <160322743+RaoNaila@users.noreply.github.com>
2024-04-21 21:11:00 -0700

    my number guess game

---
+++ main.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const systemGenerateNo = Math.floor(Math.random() * 10) + 1;
+const { userGuess } = await inquirer.prompt({
+    type: "number",
+    name: "userguess",
+    message: "Guess the number between 1 to 10:",
+});
+if (userGuess === systemGenerateNo) {
+    console.log("congraulation your guess is right");
+}
+else {
+    console.log(`oops the correct number was ${systemGenerateNo}.Good luck for next time`);
+}
